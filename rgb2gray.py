598c3abce44ff47edfce8a87cd7169936e7a3687
Paulo Serra Filho <pauloserrafh@gmail.com>
2017-03-31 23:44:06 -0300

    Croping image

--- rgb2gray.py
+++ rgb2gray.py
@@ -1,10 +1,12 @@
 #rgb2grayscale
 #https://samarthbhargav.wordpress.com/2014/05/05/image-processing-with-python-rgb-to-grayscale-conversion/
+#http://matthiaseisen.com/pp/patterns/p0202/
 
 from scipy import misc
 import matplotlib.pyplot as plt # import
 import numpy as np
 import matplotlib.cm as cm #
+from PIL import Image # Import PILLOW
 
 def weightedAverage(pixel):
 	return 0.299*pixel[0] + 0.587*pixel[1] + 0.114*pixel[2]
@@ -26,3 +28,12 @@ for rownum in range(len(image)):
 # plt.show()
 
 misc.imsave(image_path+image_name+'grey'+image_ext, grey)
+
+#Open image with PIL to crop
+image = Image.open(image_path+image_name+'grey'+image_ext)
+width = image.size[0]
+height = image.size[1]
+
+image.crop((20,15, 45, 45))
+plt.imshow(image)
+plt.show()
